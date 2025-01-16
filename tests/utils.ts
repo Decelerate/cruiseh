8a50cb5e7c66e0dbdaa5f9b2f910c7ac0188882e
cotontigeh <cotontigeh@gmail.com>
2025-01-16 18:11:31 +0100

    feat(tests): add tests folder with router.test
    fix(fmt): fix file format

---
+++ tests/utils.ts
@@ -0,0 +1,26 @@
+/**
+ * Creates a fake Request object for testing purposes.
+ * @param {string} url - The URL of the request.
+ * @param {string} method - The HTTP method of the request.
+ * @param {Record<string, string>} [headers] - Optional headers for the request.
+ * @param {any} [body] - Optional body for the request.
+ * @returns {Request} - The fake Request object.
+ */
+export function createFakeRequest(
+  url: string,
+  method: string,
+  headers: Record<string, string> = {},
+  body: unknown = null,
+): Request {
+  const init: RequestInit = {
+    method,
+    headers: new Headers(headers),
+  };
+
+  if (body) {
+    init.body = JSON.stringify(body);
+    (init.headers as Headers).set("Content-Type", "application/json");
+  }
+
+  return new Request(url, init);
+}
